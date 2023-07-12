79939fcb2f69988fd2eb388c5e9782d8e1bcb513
StevenHuang5v <htcannotregister@163.com>
2023-07-12 23:41:06 +0800

    update file3

--- sub_file3.py
+++ sub_file3.py
@@ -2,16 +2,10 @@
 self.u_slv.s_axi_awid += Combine(UInt(self.u_slv.s_axi_awid.width-self.u_mst.m_axi_awid.width,0), self.u_mst.m_axi_awid)
 self.u_slv.s_axi_arid += self.u_mst.m_axi_arid[self.u_slv.s_axi_arid.width-1:0]
 
-# get io list
-mst_list = self.u_mst.get_io('m_')
-slv_list = self.u_slv.get_io('s_')
-top_list = self.u_slv.get_io('top_')
-
-# remove io which name contains arid or awid from mst_list and slv_list
-
-exclude_io(mst_list, ['arid','awid'])
-exclude_io(slv_list, ['arid','awid'])
-exclude_io(top_list, ['awready'])
+# get io list and remove io which name contains arid or awid from mst_list and slv_list
+mst_list = exclude_io(self.u_mst.get_io('m_'), ['arid','awid'])
+slv_list = exclude_io(self.u_slv.get_io('s_'), ['arid','awid'])
+top_list = exclude_io(self.u_slv.get_io('top_'), ['awready'])
 
 # combinational logic operation in top module
 self.top_demo_signal += And(self.u_mst.m_axi_awvalid, Equal(self.u_mst.m_axi_awid, UInt(self.u_mst.m_axi_awid.width, 1)))
